bb3d36baf4ace0afce87066d24e228d29fa0feb5
PaulMcPhee79 <pc.mcphee@gmail.com>
2016-12-06 08:11:11 +1000

    Removed unnecessary files

--- CutlassCove/Raw Resources/Scripts/duplicates.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Paste into command shell to search for duplicate texture names.
-ls -R | sort | uniq -c | grep '\<[2-9][0-9]*[ ].*$'
-
-# To find path of listed duplicates
-find . -name <file name here>
